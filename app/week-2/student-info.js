9e1d2f88bad90bea5bc8f7acb0e0cfc009bbcf02
Amandeep <deepbatth20004@gmail.com>
2024-09-25 15:53:13 -0600

    week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Amandeep Singh</p>
+
+      <Link href="https://github.com/Amanbatth295/cprg306-assignments">
+      https://github.com/Amanbatth295/cprg306-assignments
+      </Link>
+    </main>
+  );
+}
