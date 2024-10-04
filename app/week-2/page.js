ef5ca4175ca9ba9aa7d8e97ccd99fac4b6372074
Amandeep <deepbatth20004@gmail.com>
2024-10-04 14:27:38 -0600

    week-3

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,7 @@
-import StudentInfo from "./student-info";
 export default function Page() {
-  return (
-    <main>
-      <p>Shopping List</p>
-      <StudentInfo />
-    </main>
-  );
-}
+    return (
+      <main>
+        <h1>Shopping List</h1>
+      </main>
+    );
+  }
